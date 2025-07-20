c0c1dc55d85cd41428b486fd2ae19ace1be679dd
sopian <sopian@eluon.com>
2025-07-20 17:00:03 +0700

    delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-from src.model import train_model
-
-if __name__ == '__main__':
-    print("Training model...")
-    model, vectorizer, X_test, y_test, y_pred = train_model()
-    print("Model selesai dilatih.")
